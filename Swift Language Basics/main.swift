d6878ed7b9285c6c3957f1117932c48325870292
Nathan <nas4m7@mail.missouri.edu>
2017-09-06 20:44:23 -0500

    added comparisons

--- Swift Language Basics/main.swift
+++ Swift Language Basics/main.swift
@@ -18,6 +18,61 @@ var distance: Double = 129.763001
 var lost: Bool = true
 var expensive: Bool = true
 var choice: Int = 2
-let integral: Character = "⧵u{222B}"
+let integral: Character = "\u{222B}"
 let greeting: String = "Hello"
 let name: String = "Karen"
+
+if sample1 == sample2 {
+    print("The samples are equal.")
+}
+else {
+    print("The samples are not equal.")
+}
+
+if heartRate >= 40 && heartRate <= 80 {
+    print("Heart rate is normal.")
+}
+else {
+    print("Heart rate is not normal.")
+}
+
+if deposits >= 100000000 {
+    print("You are exceedingly wealthy.")
+}
+else {
+    print("Sorry you are so poor.")
+}
+
+var force: Float = mass * acceleration
+print("force = \(force)")
+
+print("\(distance) is the distance")
+
+if lost == true && expensive == true {
+    print("I am really sorry! I will get the manager.")
+}
+else if lost == true && expensive == false {
+    print("Here is coupon for 10% off.")
+}
+
+switch choice {
+    case 1: print("You chose 1.")
+    case 2: print("You chose 2.")
+    case 3: print("You chose 3.")
+    default: print("You made an unknown choice.")
+}
+
+print("\(integral) is an integral.")
+
+for i in 5...10 {
+    print("i=\(i)")
+}
+
+var age: Int = 0
+
+while age < 6 {
+    print("age = \(age)")
+    age += 1
+}
+
+print("\(greeting) \(name)")
